ed5390464ae3da488d373fe7a575093b4927942c
Xu Xu <xuxucmkox@gmail.com>
2015-08-16 22:33:59 -0700

    dynamic graph cut added

---
+++ modules/TestModule2/setup.py
@@ -0,0 +1,15 @@
+# Install script for GraphcutSegmentation
+import sys
+from bq.setup.module_setup import matlab_setup, read_config, ensure_matlab, mex_compile
+
+def setup(params, *args, **kw):
+    ensure_matlab(params)
+    #mex_compile(['vrl_gc.cpp'], where='vrl_tools/maxflow_kolmogorov')
+    return matlab_setup(['GraphcutSegmentation'], params=params)
+    
+if __name__ =="__main__":
+    params = read_config('runtime-bisque.cfg')
+    if len(sys.argv)>1:
+        params = eval (sys.argv[1])
+
+    sys.exit(setup(params))
