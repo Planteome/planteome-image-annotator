f0430314c33fec060aa247011fc12df659717d07
xuxucmkox <xuxucmkox@gmail.com>
2015-10-25 21:07:09 -0700

    update

--- modules/TestModule2/bqGrabCut.m
+++ modules/TestModule2/bqGrabCut.m
@@ -55,3 +55,5 @@ function seg_fg = bqGrabCut(session, I, I_d, featChoice, noBins, nlink_sigma, in
                 [edges-1, interaction_cost * weights]' ...
                 ) , lattice_size(1), lattice_size(2) );
 end
+
+%update 10/25/2015
