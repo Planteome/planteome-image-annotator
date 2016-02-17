619e7ed73af7fbe4b6d4874f0b04da65a5792327
xuxucmkox <xuxucmkox@gmail.com>
2016-02-16 21:17:25 -0800

    module name change
    any "ImageMatting" should be replaced by "PlanteomeSegmenter"

--- modules/PlanteomeSegmenter/setup.py
+++ modules/PlanteomeSegmenter/setup.py
@@ -1,11 +1,11 @@
-# Install script for ImageMatting
+# Install script for PlanteomeSegmenter
 import sys
 from bq.setup.module_setup import matlab_setup, read_config, ensure_matlab, mex_compile
 
 def setup(params, *args, **kw):
     ensure_matlab(params)
     mex_compile(['vrl_gc.cpp'], where='vrl_tools/maxflow_kolmogorov')
-    return matlab_setup(['ImageMatting', '-a', 'vrl_tools'], params=params)
+    return matlab_setup(['PlanteomeSegmenter', '-a', 'vrl_tools'], params=params)
     
 if __name__ =="__main__":
     params = read_config('runtime-bisque.cfg')
